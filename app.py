fe535605e236d03ca1df377c9472b812c103eec7
Ivan Grachev <ivan@ivan.lol>
2020-05-18 23:10:23 +0300

    Update return value

--- app.py
+++ app.py
@@ -6,12 +6,10 @@ app = Flask(__name__)
     
 @app.route('/winner-of/<string:id>')
 def get_winner(id):
-    return '"ivan"'
-
     response = requests.get('https://kahoot.it/rest/challenges/' + id + '/progress')
     challenge = json.loads(response.text)
     leaders = challenge['leaderboard']['leaders']
     if len(leaders) > 0:
-        return leaders[0]['playerId']
+        return '"' + leaders[0]['playerId'] + '"'
     else:
         abort(400)
