2f396dc59d232fc72431075c2484a79b2ef55179
Ivan Grachev <ivan@ivan.lol>
2020-05-18 21:30:42 +0300

    Update for Heroku

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask
 import json
 import requests
-from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
-app.wsgi_app = ProxyFix(app.wsgi_app)
     
 @app.route('/winner-of/<string:id>')
 def get_winner(id):
