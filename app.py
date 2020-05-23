b95c58b70dfca0c51a23215063f0dd2f88aceba1
Vadim Zakharenko <vadim_zakhar@mail.ru>
2020-05-23 18:28:28 +0300

    Handle names.

--- app.py
+++ app.py
@@ -7,10 +7,22 @@ app = Flask(__name__)
     
 @app.route('/')
 def get_winner():
-    names = request.args.get('names')
+    argNamesRaw = str(request.args.get('names')).split(';')
+    argNames = [name.lower() for name in argNamesRaw]
     id = request.args.get('id')
     response = requests.get('https://kahoot.it/rest/challenges/' + id + '/progress')
     challenge = json.loads(response.text)
+    players = challenge['leaderboard']['players']
+    playerNames = []
+    hasUnfinished = False
+    for player in players:
+    	playerNames.append(player['playerId'].lower())
+    	hasUnfinished |= player['gameUnfinished']
+    if set(argNames) != set(playerNames):
+    	return ""
+    if hasUnfinished:
+    	abort(400)
+
     leaders = challenge['leaderboard']['leaders']
     if len(leaders) > 0:
         return '"' + leaders[0]['playerId'] + '"'
