2d7632c53190dfa7eef7b91a561e4db4b16e59d4
Vadim Zakharenko <vadim_zakhar@mail.ru>
2020-05-23 18:49:03 +0300

    Compare names from contract and api.

--- app.py
+++ app.py
@@ -15,12 +15,16 @@ def get_winner():
     players = challenge['leaderboard']['players']
     playerNames = []
     hasUnfinished = False
+    hasMultipleGamesPlayed = False
     for player in players:
     	playerNames.append(player['playerId'].lower())
     	hasUnfinished |= player['gameUnfinished']
-    if set(argNames) != set(playerNames):
+    	hasMultipleGamesPlayed |= (player['gamesPlayed'] > 1)
+    playerNamesSet = set(playerNames)
+    argNamesSet = set(argNames)
+    if hasMultipleGamesPlayed or (len(playerNamesSet.difference(argNames)) > 0):
     	return ""
-    if hasUnfinished:
+    if hasUnfinished or (len(argNamesSet) != len(playerNamesSet)):
     	abort(400)
 
     leaders = challenge['leaderboard']['leaders']
